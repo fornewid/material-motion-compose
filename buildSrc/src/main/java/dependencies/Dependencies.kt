4cf3760665a2e8c7bfecd83ee37588a51cae8c34
SOUP <soupyong@gmail.com>
2021-03-25 21:01:15 +0900

    Update to Compose 1.0.0-beta03

--- buildSrc/src/main/java/dependencies/Dependencies.kt
+++ buildSrc/src/main/java/dependencies/Dependencies.kt
@@ -18,8 +18,8 @@ object Libs {
     }
 
     object Compose {
-        const val version = "1.0.0-beta02"
-        const val activity = "androidx.activity:activity-compose:1.3.0-alpha04"
+        const val version = "1.0.0-beta03"
+        const val activity = "androidx.activity:activity-compose:1.3.0-alpha05"
         const val constraintlayout =
             "androidx.constraintlayout:constraintlayout-compose:1.0.0-alpha05"
         const val navigation = "androidx.navigation:navigation-compose:1.0.0-alpha09"
