c4637c7d41172818557e81018907ce2625cfdba0
SOUP <soupyong@gmail.com>
2021-05-19 16:45:51 +0900

    Use android gradle 7.0.0-beta01

--- buildSrc/src/main/java/dependencies/Dependencies.kt
+++ buildSrc/src/main/java/dependencies/Dependencies.kt
@@ -8,7 +8,7 @@ object Versions {
 }
 
 object Libs {
-    const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.0-alpha15"
+    const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.0-beta01"
     const val material = "com.google.android.material:material:1.3.0"
 
     object Kotlin {
