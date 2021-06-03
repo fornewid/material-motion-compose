6d2356badb9b1bffd5dd259be453d2f93c203e55
SOUP <soupyong@gmail.com>
2021-06-04 02:15:21 +0900

    Update to Compose 1.0.0-beta08 (#32)

--- buildSrc/src/main/java/dependencies/Dependencies.kt
+++ buildSrc/src/main/java/dependencies/Dependencies.kt
@@ -8,21 +8,21 @@ object Versions {
 }
 
 object Libs {
-    const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.0-beta02"
+    const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.0-beta03"
     const val material = "com.google.android.material:material:1.3.0"
 
     object Kotlin {
-        private const val version = "1.4.32"
+        private const val version = "1.5.10"
         const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
         const val plugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
     }
 
     object Compose {
-        const val version = "1.0.0-beta07"
-        const val activity = "androidx.activity:activity-compose:1.3.0-alpha08"
+        const val version = "1.0.0-beta08"
+        const val activity = "androidx.activity:activity-compose:1.3.0-beta01"
         const val constraintlayout =
             "androidx.constraintlayout:constraintlayout-compose:1.0.0-alpha07"
-        const val navigation = "androidx.navigation:navigation-compose:2.4.0-alpha01"
+        const val navigation = "androidx.navigation:navigation-compose:2.4.0-alpha02"
         const val runtime = "androidx.compose.runtime:runtime:$version"
         const val runtime_livedata = "androidx.compose.runtime:runtime-livedata:$version"
         const val foundation = "androidx.compose.foundation:foundation:$version"
