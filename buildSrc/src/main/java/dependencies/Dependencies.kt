551f83bd5ecda7c2f716627c9d47b3c3babf835e
SOUP <soupyong@gmail.com>
2021-08-05 16:59:58 +0900

    Update to Compose 1.0.1

--- buildSrc/src/main/java/dependencies/Dependencies.kt
+++ buildSrc/src/main/java/dependencies/Dependencies.kt
@@ -12,17 +12,17 @@ object Libs {
     const val material = "com.google.android.material:material:1.4.0"
 
     object Kotlin {
-        private const val version = "1.5.10"
+        private const val version = "1.5.21"
         const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
         const val plugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
     }
 
     object Compose {
-        const val version = "1.0.0"
-        const val activity = "androidx.activity:activity-compose:1.3.0"
+        const val version = "1.0.1"
+        const val activity = "androidx.activity:activity-compose:1.3.1"
         const val constraintlayout =
             "androidx.constraintlayout:constraintlayout-compose:1.0.0-alpha07"
-        const val navigation = "androidx.navigation:navigation-compose:2.4.0-alpha05"
+        const val navigation = "androidx.navigation:navigation-compose:2.4.0-alpha06"
         const val runtime = "androidx.compose.runtime:runtime:$version"
         const val runtime_livedata = "androidx.compose.runtime:runtime-livedata:$version"
         const val foundation = "androidx.compose.foundation:foundation:$version"
