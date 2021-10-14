71a1718ba20884d225c9082feb71cef0fff9509d
Sungyong An (SOUP) <soupyong@gmail.com>
2021-10-14 23:56:44 +0900

    Update to Compose 1.1.0-alpha06 (#96)
    * Update to Compose 1.1.0-alpha06
    
    * Update to Android Gradle Plugin 7.0.3

--- buildSrc/src/main/java/dependencies/Dependencies.kt
+++ buildSrc/src/main/java/dependencies/Dependencies.kt
@@ -7,7 +7,7 @@ object Versions {
 }
 
 object Libs {
-    const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.2"
+    const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.3"
     const val material = "com.google.android.material:material:1.4.0"
 
     object Kotlin {
@@ -17,7 +17,7 @@ object Libs {
     }
 
     object Compose {
-        const val version = "1.1.0-alpha05"
+        const val version = "1.1.0-alpha06"
         const val activity = "androidx.activity:activity-compose:1.3.1"
         const val constraintlayout =
             "androidx.constraintlayout:constraintlayout-compose:1.0.0-beta02"
