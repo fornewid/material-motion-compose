b8d374ca8c4d201418266b9eaf18f376c21a63fc
SOUP <soupyong@gmail.com>
2021-10-14 23:26:48 +0900

    Update to Compose 1.1.0-alpha06

--- buildSrc/src/main/java/dependencies/Dependencies.kt
+++ buildSrc/src/main/java/dependencies/Dependencies.kt
@@ -17,7 +17,7 @@ object Libs {
     }
 
     object Compose {
-        const val version = "1.1.0-alpha05"
+        const val version = "1.1.0-alpha06"
         const val activity = "androidx.activity:activity-compose:1.3.1"
         const val constraintlayout =
             "androidx.constraintlayout:constraintlayout-compose:1.0.0-beta02"
