0a31618d2fee5d2cb23d784b433b1922bdc0f085
SOUP <soupyong@gmail.com>
2022-05-24 19:42:52 +0900

    Updates for site

--- generate_docs.sh
+++ generate_docs.sh
@@ -6,4 +6,8 @@ set -ex
 
 # Copy *.md files into docs directory
 cp README.md docs/index.md
-cp docs/header.png docs/docs/header.png
+
+sed -i.bak 's/docs\/header.png/header.png/' docs/index.md
+
+# Finally delete all of the backup files
+find . -name '*.bak' -delete
