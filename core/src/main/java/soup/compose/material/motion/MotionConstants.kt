25fb99b0b5140335c46a1bba465075748140bf20
Sungyong An <soupyong@gmail.com>
2022-11-23 00:54:37 +0900

    [Core] Remove deprecated APIs (#178)

--- core/src/main/java/soup/compose/material/motion/MotionConstants.kt
+++ core/src/main/java/soup/compose/material/motion/MotionConstants.kt
@@ -25,17 +25,4 @@ public object MotionConstants {
     public const val DefaultFadeInDuration: Int = 150
     public const val DefaultFadeOutDuration: Int = 75
     public val DefaultSlideDistance: Dp = 30.dp
-
-    @Deprecated("Don't use this constant!", level = DeprecationLevel.ERROR)
-    public const val motionDurationShort1: Int = DefaultFadeOutDuration
-    @Deprecated("Don't use this constant!", level = DeprecationLevel.ERROR)
-    public const val motionDurationShort2: Int = DefaultFadeInDuration
-    @Deprecated("Don't use this constant!", level = DeprecationLevel.ERROR)
-    public const val motionDurationMedium1: Int = 200
-    @Deprecated("Don't use this constant!", level = DeprecationLevel.ERROR)
-    public const val motionDurationMedium2: Int = 250
-    @Deprecated("Don't use this constant!", level = DeprecationLevel.ERROR)
-    public const val motionDurationLong1: Int = DefaultMotionDuration
-    @Deprecated("Don't use this constant!", level = DeprecationLevel.ERROR)
-    public const val motionDurationLong2: Int = 350
 }
