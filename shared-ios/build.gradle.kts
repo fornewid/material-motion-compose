057dbde3f4bc0258d0f05a2c2112047c9025006f
Sungyong An <soupyong@gmail.com>
2023-04-26 17:53:54 +0900

    Fix build errors on iOS

--- shared-ios/build.gradle.kts
+++ shared-ios/build.gradle.kts
@@ -28,5 +28,14 @@ kotlin {
                 implementation(project(":shared"))
             }
         }
+        val iosX64Main by getting
+        val iosArm64Main by getting
+        val iosSimulatorArm64Main by getting
+        val iosMain by creating {
+            dependsOn(commonMain)
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
+        }
     }
 }
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -28,6 +28,15 @@ kotlin {
                 implementation(compose.desktop.common)
             }
         }
+        val iosX64Main by getting
+        val iosArm64Main by getting
+        val iosSimulatorArm64Main by getting
+        val iosMain by creating {
+            dependsOn(commonMain)
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
+        }
     }
 }
 
